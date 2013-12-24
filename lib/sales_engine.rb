c0b6d285877c4fdae284c2f758aed6121fa45901
Matt Sroufe <matt.sroufe@powershop.co.nz>
2013-12-24 17:32:57 +1300

    adds SalesEngine#market_repository and spec

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,4 +3,9 @@ require 'sales_engine/merchant'
 require 'sales_engine/merchant_repository'
 
 class SalesEngine
+  attr_reader :merchant_repository
+
+  def initialize
+    @merchant_repository = MerchantRepository.new
+  end
 end
--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -1,8 +1,15 @@
 describe SalesEngine do
+  let(:engine) { SalesEngine.new }
 
   describe ".new" do
     it "creates a new SalesEngine instance" do
-      expect(SalesEngine.new).to be_an_instance_of SalesEngine
+      expect(engine).to be_an_instance_of SalesEngine
+    end
+  end
+
+  describe "#merchant_repository" do
+    it "returns an instance of MerchantRepository" do
+      expect(engine.merchant_repository).to be_an_instance_of MerchantRepository
     end
   end
 end

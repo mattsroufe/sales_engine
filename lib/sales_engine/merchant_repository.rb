c71703ff74bdd9bcbf8270358590cb461068a257
Matt Sroufe <matt.sroufe@powershop.co.nz>
2014-06-24 19:35:00 +1200

    adds map for merchant_repository

--- lib/sales_engine/merchant_repository.rb
+++ lib/sales_engine/merchant_repository.rb
@@ -11,10 +11,8 @@ class MerchantRepository
   private
 
   def parse_csv(file)
-    array = []
-    CSV.foreach(file, :headers => true, :header_converters => :symbol, :converters => :integer) do |row|
-      array << Merchant.new(row[:id], row[:name], Time.parse(row[:created_at]), Time.parse(row[:updated_at]))
+    CSV.read(file, :headers => true, :header_converters => :symbol, :converters => :integer).map do |row|
+      Merchant.new(row[:id], row[:name], Time.parse(row[:created_at]), Time.parse(row[:updated_at]))
     end
-    array
   end
 end

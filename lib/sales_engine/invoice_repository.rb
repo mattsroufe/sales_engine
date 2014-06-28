3d175b84789c520e963bb4648e0ac961f14c71fd
Matt Sroufe <matt.sroufe@powershop.co.nz>
2014-06-29 11:26:38 +1200

    wip

--- lib/sales_engine/invoice_repository.rb
+++ lib/sales_engine/invoice_repository.rb
@@ -3,6 +3,10 @@ class InvoiceRepository
 
   CSV_DATA_FILE = 'data/invoices.csv'
 
+  def create(args = {})
+    Invoice.new(repository: self, id: customer_id: merchant_id:)
+  end
+
   def find_all_by_merchant_id(merchant_id)
     all.select { |invoice| invoice.merchant_id == merchant_id }
   end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -10,4 +10,28 @@ FactoryGirl.define do
 
     initialize_with { new(attributes) }
   end
+
+  factory :item2, class: Item do
+    id 2
+    name 'Item Autem Minima'
+    description 'Cumque consequuntur ad. Fuga tenetur illo molestias enim aut iste. Provident quo hic aut. Aut quidem voluptates dolores. Dolorem quae ab alias tempora.'
+    unit_price 67076
+    merchant_id 1
+    created_at Time.new(2014, 6, 1)
+    updated_at Time.new(2014, 6, 1)
+
+    initialize_with { new(attributes) }
+  end
+
+  factory :item3, class: Item do
+    id 3
+    name 'Item Ea Voluptatum'
+    description 'Sunt officia eum qui molestiae. Nesciunt quidem cupiditate reiciendis est commodi non. Atque eveniet sed. Illum excepturi praesentium reiciendis voluptatibus eveniet odit perspiciatis. Odio optio nisi rerum nihil ut.'
+    unit_price 32301
+    merchant_id 1
+    created_at Time.new(2014, 6, 1)
+    updated_at Time.new(2014, 6, 1)
+
+    initialize_with { new(attributes) }
+  end
 end
